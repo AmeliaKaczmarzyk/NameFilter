5b83bfe71f98701aaf6db9e339ceffd49a062de5
Marin Kitagawa <marinkitagawa.x86@gmail.com>
2022-09-14 13:38:41 -0400

    Updated for functionality

--- src/NameFilter.java
+++ src/NameFilter.java
@@ -2,8 +2,7 @@ import java.util.Scanner;
 
 public class NameFilter
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws InterruptedException {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter your first name");
         String firstName = in.nextLine();
@@ -18,7 +17,15 @@ public class NameFilter
         }
         else
         {
-            System.out.println("Why the heck would anyone be named " + firstName + " " + lastName + " ? What a terrible name!");
+            System.out.println("Ew, who names their child " +
+                    firstName + " " + lastName + " ? What a terrible name!");
+            //Thread.sleep(4200, 69);
+            for(int i = 0; i < 11; i++)
+            {
+                System.out.println(".");
+                Thread.sleep(420, 69);
+            }
+            System.out.println("Jk man, " + firstName + " " + lastName + " is a great name, just not as good as Mr. Cochran's name!");
         }
     }
 }
