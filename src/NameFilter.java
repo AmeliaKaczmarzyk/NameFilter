88b3b18fcc11b8b01fd04724f582de6141b05969
Marin Kitagawa <marinkitagawa.x86@gmail.com>
2022-09-14 13:47:56 -0400

    New elif statement

--- src/NameFilter.java
+++ src/NameFilter.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class NameFilter
 {
     public static void main(String[] args) throws InterruptedException {
+
         Scanner in = new Scanner(System.in);
         System.out.println("Enter your first name");
         String firstName = in.nextLine();
@@ -15,11 +16,18 @@ public class NameFilter
         {
             System.out.println(firstName + " " +  lastName + " is a great name!");
         }
+        else if(firstName.equals("Marin") || firstName.equals("Lucy")){
+            System.out.println("I know that isn't really your name...");
+            Thread.sleep(420, 69);
+            System.out.println("... You better stop watching so much anime, and definitely don't watch that horrible Elfen Lied series!");
+        }
+
         else
         {
             System.out.println("Ew, who names their child " +
-                    firstName + " " + lastName + " ? What a terrible name!");
+                    firstName + " " + lastName + "? What a terrible name!");
             //Thread.sleep(4200, 69);
+            Thread.sleep(1200);
             for(int i = 0; i < 11; i++)
             {
                 System.out.println(".");
